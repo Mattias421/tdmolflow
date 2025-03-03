0e81a1cca2b1cca52c35e2f1375adbc9df804012
mattias421 <mcross2@sheffield.ac.uk>
2025-03-03 13:33:45 +0000

    update submit cfm

--- submit_cfm.sh
+++ submit_cfm.sh
@@ -4,7 +4,8 @@
 #SBATCH --qos=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --mem=16G
-#SBATCH --time=80:00:00
+#SBATCH --time=168:00:00
+#SBATCH --array=0-0
 
 module load Anaconda3/2019.07
 
@@ -13,6 +14,10 @@ source activate tdmolflow
 export WANDB_ENTITY=mattias421
 export WANDB_PROJECT=tdmolflow
 
+sigmas=(0.1 0.2 0.3)
+
+sigma=${sigmas[$SLURM_ARRAY_TASK_ID]}
+
 python train.py --workers 8 --sample 50 --batch 64 --lr 0.00003 --ema 0.5 \
     --observed 0,0,0,1,1,1,1,1,1 --exist 1,1,1,1,1,1,1,1,1 --snap 25 --dump 25 \
     --precond eps --data_class QM9Dataset --qm9dataset_shuffle_node_ordering True \
@@ -20,7 +25,7 @@ python train.py --workers 8 --sample 50 --batch 64 --lr 0.00003 --ema 0.5 \
     --qm9dataset_atom_type_norm 0.25 --loss_class JumpLossFinalDim \
     --jumplossfinaldim_rate_function_name step --jumplossfinaldim_rate_cut_t 0.1 \
     --jumplossfinaldim_mean_or_sum_over_dim mean --jumplossfinaldim_noise_schedule_name cfm_ode \
-    --jumplossfinaldim_vp_sde_beta_min 0.1 \
+    --jumplossfinaldim_vp_sde_beta_min $sigma \
     --jumplossfinaldim_x0_logit_ce_loss_weight 1.0 --jumplossfinaldim_nearest_atom_pred True \
     --sampler_class JumpSampler --jumpsampler_sample_near_atom True --network_class EGNNMultiHeadJump \
     --egnnmultiheadjump_detach_last_layer True --egnnmultiheadjump_rate_use_x0_pred True \
